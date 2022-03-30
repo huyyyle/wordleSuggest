63a22b4e0a6f44f1d9a96f9bb4c0c2892b565623
Huy <huyyyle@gmail.com>
2022-03-29 19:54:50 -0700

    Edited code for clarity.

--- wordlesuggest.js
+++ wordlesuggest.js
@@ -1,14 +1,10 @@
-//import wordleSuggestController from './scr/controller'
-//import wordleSuggestModel from './scr/model'
-//import wordleSuggestView from './scr/view'
-
 function main() {
     model = new wordleSuggestModel();
     controller = new wordleSuggestController(model);
     view = new wordleSuggestView(model, controller);
     view.initializeView();
-}
 
+}
 document.addEventListener('DOMContentLoaded', (event) => {main()});
 
 
