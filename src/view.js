e7eb1b89a6063d66831bdfa9da302474609b0094
Huy <huyyyle@gmail.com>
2022-03-29 19:54:14 -0700

    Added the ability to press Tab to cycle through suggestions.

--- src/view.js
+++ src/view.js
@@ -15,17 +15,28 @@ class wordleSuggestView {
         this.addCSS();
     }
 
+    /**
+     * Create the initial view, and initialize basic
+     * variables. Initializes input handling.
+     */
     initializeView() {
         this.#currentRow = this.getCurRow();
         this.#currentGuess = this.getCurGuess();
         this.keyboardEventHandling();
     }
 
+    /**
+     * Updates the model through the controller
+     * everytime a key is pressed. Moves focus
+     * to the row below when enter is pressed.
+     */
     keyboardEventHandling() {
         document.addEventListener('keyup', (event) => {
             if (event.key == 'Enter') {
-                console.log("ENTER EVENT HANDLED");
                 this.updateRow();
+            }
+            else if (event.key == 'Tab') {
+                this.nextSuggestion();
             }
             else {
                 this.updateView();
@@ -35,20 +46,33 @@ class wordleSuggestView {
 
     }
 
+    /**
+     * Updates the current row selector.
+     */
     updateRow() {
         this.#currentRow = this.getCurRow();
     }
 
+    /**
+     * Updates the suggestion shown on screen.
+     * @returns null;
+     */
     updateView() {
         this.#currentGuess = this.getCurGuess();
-        if (this.#currentGuess.length == 0 || this.#currentGuess.length == 5) {
+        if (this.#currentGuess.length == 0) {
             this.clearSuggestion();
+            return;
+        }
+        if (this.#currentGuess.length == 5) {
             return;
         }
         this.displaySuggestion();
 
     }
 
+    /**
+     * Clears the suggestion bar.
+     */
     clearSuggestion() {
         var tiles = this.#currentRow.querySelector('.row').querySelectorAll('game-tile');
         for (var i = this.#model.getDictStack().length; i < 5; i++) {
@@ -56,6 +80,11 @@ class wordleSuggestView {
         }
     }
 
+    /**
+     * Show an autocomplete on screen for the first
+     * word possible from the current guess.
+     * @returns 
+     */
     displaySuggestion() {
         var tiles = this.#currentRow.querySelector('.row').querySelectorAll('game-tile');
         this.#controller.makeGuess(this.#currentGuess);
@@ -66,12 +95,28 @@ class wordleSuggestView {
             return;
         }
         //TODO: ADD A WAY TO GO TO NEXT IN DICT
-        var suggestion = curDict[0];
+        var suggestion = this.#controller.getSuggestion();
         for (var i = this.#model.getDictStack().length; i < 5; i++) {
-            this.addSuggestion(tiles[i], curDict[0].charAt(i - this.#model.getDictStack().length))
+            this.addSuggestion(tiles[i], suggestion.charAt(i - this.#model.getDictStack().length))
         }
     }
 
+    nextSuggestion() {
+        var tiles = this.#currentRow.querySelector('.row').querySelectorAll('game-tile');
+        var suggestion = this.#controller.getSuggestion();
+        var curDict = this.#model.peekDictStack();
+        if (curDict.length == 0) {
+            return;
+        }
+        for (var i = this.#model.getDictStack().length; i < 5; i++) {
+            this.addSuggestion(tiles[i], suggestion.charAt(i - this.#model.getDictStack().length))
+        }
+    }
+
+    /**
+     * Targets and returns the element containing the game.
+     * @returns the element containing the Wordle Board.
+     */
     getInjectionSite() {
         var x = document.querySelector("game-app");
         x = x.shadowRoot;
@@ -83,6 +128,10 @@ class wordleSuggestView {
         return x;
     }
 
+    /**
+     * Get the guess row the player is currently on.
+     * @returns the element containg the ongoing guess.
+     */
     getCurRow() {
         var curRow;
         for (const element of this.#injectionSite) {
@@ -95,16 +144,30 @@ class wordleSuggestView {
         }
     }
 
+    /**
+     * Convert a given tile to a "suggestion" tile
+     * with the given letter.
+     * @param {*} tile element containing the tile.
+     * @param {*} letter one lettered String.
+     */
     addSuggestion(tile, letter) {
         var innerTile = tile.shadowRoot.querySelector('div');
         innerTile.innerHTML = letter;
         innerTile.setAttribute('data-state', 'suggest');
     }
 
+    /**
+     * Convert a given tile back to the empty state.
+     * @param {*} tile element containing the tile.
+     */
     removeSuggestion(tile) {
         tile.shadowRoot.querySelector('div').setAttribute('data-state', 'empty');
     }
 
+    /**
+     * Adds the custom CSS for a suggestive Wordle tile, to
+     * every tile.
+     */
     addCSS() {
         for (const element of this.#injectionSite) {
             var tiles = element.shadowRoot.querySelector('.row').querySelectorAll('game-tile');
@@ -118,6 +181,10 @@ class wordleSuggestView {
         }
     }
 
+    /**
+     * Get the ongoing user guess.
+     * @returns the String of the current user guess.
+     */
     getCurGuess() {
         var tiles = this.#currentRow.querySelector('.row').querySelectorAll('game-tile');
         var guess = '';
