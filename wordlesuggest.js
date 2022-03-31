3bfba7b7e4f7cbac1e4bae7f24973f2b99b3b959
Huy <huyyyle@gmail.com>
2022-03-30 23:10:13 -0700

    Formatted for better readability.

--- wordlesuggest.js
+++ wordlesuggest.js
@@ -2,8 +2,5 @@ function main() {
     model = new wordleSuggestModel();
     controller = new wordleSuggestController(model);
     view = new wordleSuggestView(model, controller);
-
 }
 document.addEventListener('DOMContentLoaded', (event) => {main()});
-
-
