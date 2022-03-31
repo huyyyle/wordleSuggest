6e0055efa9a12c0ca8b7e9074e3a18c3f8043960
Huy <huyyyle@gmail.com>
2022-03-30 22:57:01 -0700

    Added documentation. Removed artifacts from when suggestion selection was moved to View.

--- src/controller.js
+++ src/controller.js
@@ -1,16 +1,19 @@
 class wordleSuggestController {
     #model;
-    #dictIndex;
-
 
     constructor(wordleSuggestModel) {
         this.#model = wordleSuggestModel;
         this.#dictIndex = 0;
     }
     
+    /**
+     * Update the dictionary stack in the model
+     * to reflect the possible words given
+     * what the user has given so far.
+     * @param {*} guess is the String of the ongoing guess.
+     */
     makeGuess(guess) {
         this.#model.clearGuess();
-        this.#dictIndex = 0;
         for (let index = 0; index < guess.length; index++) {
             const letter = guess[index];
             this.#model.addGuess(letter);
