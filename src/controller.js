b14b9011f5d11fd229d43d53b8f040570f54614e
Huy <huyyyle@gmail.com>
2022-03-31 13:26:36 -0700

    Optimized to take advantage of the dictionary stack.

--- src/controller.js
+++ src/controller.js
@@ -3,7 +3,6 @@ class wordleSuggestController {
 
     constructor(wordleSuggestModel) {
         this.#model = wordleSuggestModel;
-        this.#dictIndex = 0;
     }
     
     /**
@@ -13,10 +12,23 @@ class wordleSuggestController {
      * @param {*} guess is the String of the ongoing guess.
      */
     makeGuess(guess) {
-        this.#model.clearGuess();
-        for (let index = 0; index < guess.length; index++) {
-            const letter = guess[index];
-            this.#model.addGuess(letter);
+        if (guess.length == this.#model.getPreviousGuess().lenght + 1) {
+            this.#model.addGuess(guess.charAt(guess.length -1));
+            this.#model.setPreviousGuess = guess;
+        }
+        else if (guess.length == this.#model.getPreviousGuess().lenght - 1) {
+            this.#model.delGuess();
+            this.#model.setPreviousGuess = guess;
+        }
+        else {
+            this.#model.clearGuess();
+            for (let index = 0; index < guess.length; index++) {
+                const letter = guess[index];
+                this.#model.addGuess(letter);
+            }
+            this.#model.setPreviousGuess = guess;
         }
     }
+
+    
 }
